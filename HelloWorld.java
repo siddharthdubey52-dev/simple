0e5996cb131e7e0598d7503e2eaec33769736a37
Siddharth Dubey <96692596+SiddharthDubey52@users.noreply.github.com>
2025-08-25 15:19:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Kya haal chall h jenkins Poll Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
     }
 }
