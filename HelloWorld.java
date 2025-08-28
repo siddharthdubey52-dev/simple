e122925c863de20e7dcbb35b427fea893f03f02a
Siddharth Dubey <96692596+SiddharthDubey52@users.noreply.github.com>
2025-08-28 16:42:39 +0530

    Update greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World dfghjkjhgfdsa!");
         System.out.println("Hello, Kya haal chall h jenkins Poll Scm");
         System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
         System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
